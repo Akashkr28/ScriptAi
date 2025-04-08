24b2d19d342245c4099c7efa657e0e77b2b4ad82
Akashkr28 <email.akash2010@gmail.com>
2025-04-08 22:45:49 +0530

    Script AI project setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='bg-red-500'>
+      <h1 className='font-bold text-3xl'>hello</h1>
+    </div>
+  )
+}
+
+export default App
